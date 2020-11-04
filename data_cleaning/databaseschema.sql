a477fe9c8ce7a2bed2e428dc273c1d8c29edc73f
Kirstie McCown <accounts@intimemechanical.com.au>
2020-11-04 22:04:55 +0800

    decided on final data to be used from datasource and saved as csv for upload

--- data_cleaning/databaseschema.sql
+++ data_cleaning/databaseschema.sql
@@ -1,40 +1,38 @@
-create table tablename (
-	columnname varchar,
-	columnname varchar,
-	columnname varchar,
-	columnname varchar,
-	columnname varchar,
-	columnname varchar,
-	columnname varchar,
-	columnname varchar,
-	columnname varchar,
-	columnname varchar,
-	columnname varchar
+create table mothers (
+	age_group int,
+	topic varchar,
+	topic_disaggregation varchar,
+	year int,
+	count int,
+	year_total int,
+	percent_total float
 );
 
-create table tablename (
+create table babies (
 
-	columnname varchar,
-	columnname varchar,
-	columnname varchar,
-	columnname varchar,
-	columnname varchar,
-	columnname varchar,
-	columnname varchar,
-	columnname varchar,
-	columnname varchar,
-	columnname varchar,
-	columnname varchar,
-	columnname varchar,
-	columnname varchar,
-	columnname varchar,
-	columnname varchar,
-	columnname varchar,
-	columnname varchar
+	babies_term varchar,
+	topic varchar,
+	topic_disaggregation varchar,
+	year int,
+	count int,
+	year_total int,
+	percent_total float
 
 );
 
-create table tablename (
+create table birthplace (
+
+	place_of_birth varchar,
+	topic varchar,
+	topic_disaggregation varchar,
+	year int,
+	count int,
+	year_total int,
+	percent_total float
+
+);
+
+create table adoptions (
 	
 	columnname varchar,
 	columnname varchar,
