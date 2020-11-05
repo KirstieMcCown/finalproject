7e32bce4b5f6d856e97a4bb03c3e1f2222c911eb
Kirstie McCown <accounts@intimemechanical.com.au>
2020-11-05 12:44:54 +0800

    completed sql schema, loaded all tables into db for mothers, babies and adoptions, need to create dataframes for plotting

--- data_cleaning/databaseschema.sql
+++ data_cleaning/databaseschema.sql
@@ -1,3 +1,5 @@
+-- Mothers Tables -- 
+
 create table mothers (
 	index int,
 	age_group int,
@@ -31,6 +33,8 @@ create table birthlocation (
 	percent_total float
 );
 
+-- Babies Tables -- 
+
 create table babies (
 	index int,
 	babies_term varchar,
@@ -43,34 +47,130 @@ create table babies (
 
 );
 
-create table birthplace (
+-- Adoption Tables -- 
+
+create table numberchildrenadopted (
 	index int,
-	place_of_birth varchar,
-	topic varchar,
-	topic_disaggregation varchar,
 	year int,
-	count int,
-	year_total int,
-	percent_total float
-
-);
-
-create table adoptions (
-	index int,
-	columnname varchar,
-	columnname varchar,
-	columnname varchar,
-	columnname varchar,
-	columnname varchar,
-	columnname varchar,
-	columnname varchar,
-	columnname varchar,
-	columnname varchar,
-	columnname varchar,
-	columnname varchar,
-	columnname varchar,
-	columnname varchar,
-	columnname varchar
+	nsw int,
+	vic int,
+	qld int,
+	wa int,
+	sa int,
+	tas int,
+	act int,
+	nt int
 	
 );
 
+create table adoptionsbytype (
+	index int,
+	year int,
+	children_adopted_from_australia int,
+	percent_from_australia float,
+	children_adopted_from_overseas int,
+	percent_from_overseas float
+	
+);
+
+create table ageofbirthmother (
+	index int,
+	year int,
+	marital_status varchar,
+	twenty int,
+	twentyfour int,
+	twentynine int,
+	thirtyfour int,
+	thirtynine int, 
+	fourty int
+	
+);
+
+create table adoptionbyageandgender (
+	index int,
+	year int,
+	sex varchar,
+	one int,
+	four int,
+	nine int,
+	ten int,
+	total int
+	
+);
+
+create table parentrelationship (
+	index int,
+	year int,
+	step_parent int,
+	relatives int,
+	carer int,
+	other int,
+	total int
+	
+);
+
+create table adoptiveparentrelationship (
+	index int,
+	year int,
+	adoption_by_relatives int,
+	adoption_by_non_relatives int,
+	total int
+	
+);
+
+create table typeofadoption (
+	index int,
+	year int,
+	intercountry int,
+	local int,
+	known int,
+	total int
+	
+);
+
+create table intercountry_bycountryoforigin (
+	index int,
+	country_of_birth varchar,
+	year_2010 int,
+	year_2011 int,
+	year_2012 int,
+	year_2013 int,
+	year_2014 int, 
+	year_2015 int,
+	year_2016 int,
+	year_2017 int,
+	year_2018 int,
+	total int
+	
+);
+
+create table intercountry_byagegroup (
+	index int,
+	year int,
+	one int,
+	four int,
+	nine int,
+	ten int,
+	total int
+	
+);
+
+create table intercountry_bysiblinggroup (
+	index int,
+	year int,
+	number_sibling_groups int,
+	children_adopted_in_sibling_group int
+	
+);
+
+create table adoptionprocesstime (
+	index int,
+	year int,
+	age_group int,
+	months_for_approval int,
+	months_before_file_sent int,
+	months_from_file_sent_to_child_allocated int,
+	months_from_time_child_allocated_to_placed int,
+	overall_months_to_adopt int
+	
+);
