84e1ef5e799ddc427d068ff869117f7e3dfb36f2
Kirstie McCown <accounts@intimemechanical.com.au>
2020-11-05 21:55:46 +0800

    change gitignore

--- html/static/js/test.js
+++ html/static/js/test.js
@@ -3,7 +3,10 @@ console.log("Hello")
 
 const url = "/data";
 
-d3.json(url).then(function(data) {
-  console.log(data);
-});
+// d3.json(url).then(function(data) {
+//   console.log(data);
+// });
 
+// Promise Pending
+const dataPromise = d3.json(url);
+console.log("Data Promise: ", dataPromise);
