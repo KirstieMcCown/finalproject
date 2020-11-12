1c02a3c8b1b7e7610c1f441bf5615e90b05f81c5
Kirstie McCown <kirstiemccown@hotmail.com>
2020-11-12 23:33:12 +0800

    heroku test

--- app.py
+++ app.py
@@ -7,11 +7,7 @@ from flask import (
     request,
     redirect)
 
-# from flask import Flask, render_template
-# from flask import Flask, jsonify
-# from sqlalchemy import create_engine
-import pandas as pd
-import json
+
 try:
     from config import username
     from config import password
@@ -20,14 +16,14 @@ try:
 except:
     print ("No config file")
 # create connection to databse
-import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
+import pandas as pd
+import json
 
 app = Flask(__name__)
 
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '') or (f"postgresql://{username}:{password}@{hostname}:5432/{database}")
-# engine = create_engine(f"postgresql://{username}:{password}@{hostname}:5432/{database}")
-# connect = engine.connect()
 
 # Remove tracking modifications
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -37,60 +33,59 @@ db = SQLAlchemy(app)
 
 
 
+# # Data Routes
 
-# Data Routes
-
-@app.route("/yearcount", methods=["GET", "POST"])
-def yearcount():
+# @app.route("/yearcount")
+# def yearcount():
     
-    data = pd.read_sql("select * from yearcount")
-    # print(data)
-    datatojson = data.to_json(orient = "records")
-    parsed = json.loads(datatojson)
-    returnjson = json.dumps(parsed, indent=4)
-    return returnjson
+#     data = pd.read_sql("select * from yearcount")
+#     # print(data)
+#     datatojson = data.to_json(orient = "records")
+#     parsed = json.loads(datatojson)
+#     returnjson = json.dumps(parsed, indent=4)
+#     return returnjson
 
 
-@app.route("/averageage", methods=["GET", "POST"])
-def averageage():
+# @app.route("/averageage")
+# def averageage():
     
-    data = pd.read_sql("select * from averageage")
-    # print(data)
-    datatojson = data.to_json(orient = "records")
-    parsed = json.loads(datatojson)
-    returnjson = json.dumps(parsed, indent=4)
-    return returnjson
+#     data = pd.read_sql("select * from averageage")
+#     # print(data)
+#     datatojson = data.to_json(orient = "records")
+#     parsed = json.loads(datatojson)
+#     returnjson = json.dumps(parsed, indent=4)
+#     return returnjson
 
 
-@app.route("/birthstate", methods=["GET", "POST"])
-def birthstate():
+# @app.route("/birthstate")
+# def birthstate():
     
-    data = pd.read_sql("select * from birthstate")
-    # print(data)
-    datatojson = data.to_json(orient = "records")
-    parsed = json.loads(datatojson)
-    returnjson = json.dumps(parsed, indent=4)
-    return returnjson
+#     data = pd.read_sql("select * from birthstate")
+#     # print(data)
+#     datatojson = data.to_json(orient = "records")
+#     parsed = json.loads(datatojson)
+#     returnjson = json.dumps(parsed, indent=4)
+#     return returnjson
 
-@app.route("/termbabiescount", methods=["GET", "POST"])
-def termbabiescount():
+# @app.route("/termbabiescount")
+# def termbabiescount():
     
-    data = pd.read_sql("select * from termbabiescount")
-    # print(data)
-    datatojson = data.to_json(orient = "records")
-    parsed = json.loads(datatojson)
-    returnjson = json.dumps(parsed, indent=4)
-    return returnjson
+#     data = pd.read_sql("select * from termbabiescount")
+#     # print(data)
+#     datatojson = data.to_json(orient = "records")
+#     parsed = json.loads(datatojson)
+#     returnjson = json.dumps(parsed, indent=4)
+#     return returnjson
 
-@app.route("/babiessexcount", methods=["GET", "POST"])
-def babiessexcount():
+# @app.route("/babiessexcount")
+# def babiessexcount():
     
-    data = pd.read_sql("select * from babiessexcount")
-    # print(data)
-    datatojson = data.to_json(orient = "split")
-    parsed = json.loads(datatojson)
-    returnjson = json.dumps(parsed, indent=4)
-    return returnjson
+#     data = pd.read_sql("select * from babiessexcount")
+#     # print(data)
+#     datatojson = data.to_json(orient = "split")
+#     parsed = json.loads(datatojson)
+#     returnjson = json.dumps(parsed, indent=4)
+#     return returnjson
 
 
 
@@ -99,50 +94,54 @@ def babiessexcount():
 @app.route("/")
 def landing():
     
-    return render_template("index.html", template_folder="templates")
+    return render_template("index.html")
 
 
-@app.route("/index.html", methods=["GET", "POST"])
+
+
+@app.route("/index.html")
 def index():
     
     return render_template("index.html")
 
-@app.route("/mothers.html", methods=["GET", "POST"])
+
+
+@app.route("/mothers.html")
 def mothers():
     
     return render_template("mothers.html")
 
-@app.route("/mothersfullmap.html", methods=["GET", "POST"])
+@app.route("/mothersfullmap.html")
 def mothersfullmap():
     
     return render_template("mothersfullmap.html")
 
-@app.route("/babies.html", methods=["GET", "POST"])
+@app.route("/babies.html")
 def babies():
     
     return render_template("babies.html")
 
-@app.route("/babiesfullmap.html", methods=["GET", "POST"])
+@app.route("/babiesfullmap.html")
 def babiesfullmap():
     
     return render_template("babiesfullmap.html")
 
-@app.route("/adoptions.html", methods=["GET", "POST"])
+@app.route("/adoptions.html")
 def adoptions():
     
     return render_template("adoptions.html")
 
-@app.route("/adoptionsfullmap.html", methods=["GET", "POST"])
+@app.route("/adoptionsfullmap.html")
 def adoptionsfullmap():
     
     return render_template("adoptionsfullmap.html")
 
-@app.route("/about.html", methods=["GET", "POST"])
+@app.route("/about.html")
 def about():
     
     return render_template("about.html")
 
-@app.route("/data.html", methods=["GET", "POST"])
+@app.route("/data.html")
 def data():
     
     return render_template("data.html")
