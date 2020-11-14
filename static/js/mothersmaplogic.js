c2ed952b35f5ed92bce0352dd0200ed768cdd1b8
Kirstie McCown <kirstiemccown@hotmail.com>
2020-11-14 12:28:24 +0800

    update

--- static/js/mothersmaplogic.js
+++ static/js/mothersmaplogic.js
@@ -1,6 +1,4 @@
 // Create base layers
-var API_KEY =
-  "pk.eyJ1Ijoia2lyc3RpZW1jY293biIsImEiOiJja2ZkdTFuZDYwN2xrMzFwNTZodmJxMHh4In0.5mpXuTewEvMddcIFIJbqOA";
   
 // Streetmap Layer
 var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
